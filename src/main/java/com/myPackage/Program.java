0eac99b1b667f53a8e7e9b316a64b533b378b030
Yiks <yikyik.beh@natwest.com>
2025-04-23 12:12:39 +0100

    initial job structure

---
+++ src/main/java/com/myPackage/Program.java
@@ -0,0 +1,6 @@
+package com.myPackage;
+public class Program {
+	public static void main(String[] args) {
+		System.out.println("This program is a Maven project and the package is com.myPackage");
+	}
+}
